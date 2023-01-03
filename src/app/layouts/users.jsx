d93ba4aff67e6479f79f9bc26f52e60137c0f833
Денис Зубрицкий <fort_zd@mail.ru>
2023-01-03 17:39:14 +0700

    Урок 20, часть 14. Загружены данные о пользователях (редирект неавторизованного пользователя на страницу входа и после входа обратно на страницу пользователей

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -1,30 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams, Redirect } from "react-router-dom";
 import EditUserPage from "../components/page/editUserPage";
 import UserPage from "../components/page/userPage";
 import UsersListPage from "../components/page/usersListPage";
 import { useAuth } from "../hooks/useAuth";
+import { getDataStatus, loadUsersList } from "../store/users";
 // import UserProvider from "../hooks/useUsers";
 const Users = () => {
     const params = useParams();
     const { userId, edit } = params;
     const { currentUser } = useAuth();
+    const dataStatus = useSelector(getDataStatus());
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if (!dataStatus) dispatch(loadUsersList());
+    }, []);
+    if (!dataStatus) return "Loading...";
     return (
         <>
             {/* <UserProvider> */}
-                {userId ? (
-                    edit ? (
-                        userId === currentUser._id ? (
-                            <EditUserPage />
-                        ) : (
-                            <Redirect to={`/users/${currentUser._id}/edit`} />
-                        )
+            {userId ? (
+                edit ? (
+                    userId === currentUser._id ? (
+                        <EditUserPage />
                     ) : (
-                        <UserPage userId={userId} />
+                        <Redirect to={`/users/${currentUser._id}/edit`} />
                     )
                 ) : (
-                    <UsersListPage />
-                )}
+                    <UserPage userId={userId} />
+                )
+            ) : (
+                <UsersListPage />
+            )}
             {/* </UserProvider> */}
         </>
     );
